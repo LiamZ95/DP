1a2ae356b28b39131dd629d47a398f5bc2a2a7e7
LiamZ <liamz95@outlook.com>
2017-09-04 23:55:07 +1000

    Mon night

--- Proj1.hs
+++ Proj1.hs
@@ -6,30 +6,91 @@ module Proj1 (initialGuess, nextGuess, GameState) where
 -- Origin:
 -- Purpose:
 --
---|This code
+-- | This code is used to generate
 
+-- | This segment of code can set GameState as a alias of type [[String]].
 
-type GameState = ([String])
+import Data.Map (fromListWith, toList)
 
-type GameState = ([String])
+type GameState = [[String]]
 
-initialGuess :: ([String], GameState)
+type Answer = (Int, Int, Int)
 
-
+-- | This function can return all possible pitches 
+-- and put them in a list without duplicates.
 generatePitch :: [String]
 generatePitch = [x ++ y | x <- note, y <- octave]
   where note = ["A", "B", "C", "D", "E", "F", "G"]
         octave = ["1", "2", "3"]
 
+-- | This function can generate a chord by selecting the first 3 pitches from
+-- the list of pitches returned by generatePitch.
 generateChord :: [String]
 generateChord = take 3 generatePitch
 
-generateAll :: [[String]]
-generateAll = [(x, y, z) | x <- all, y <- all, z <- all, ]
-  where all = generatePitch
+-- | This function returns all possible combinations of 3 pitches without duplicates,
+-- the sum of combinations is 1330.
+generateAllChord :: [[String]]
+generateAllChord = [[x, y, z] | x <- all, y <- all, z <- all, x < y, y < z]
+  where all = generatePitch 
+
+-- | This function can remove a element from a list. It returns a new list of type a
+-- without the a with value you have specified.
+deleteElem :: Eq a => a -> [a] -> [a]
+deleteElem _ [] = []
+deleteElem a (x:xs)
+  | a == x = xs
+  | otherwise = x : deleteElem a xs
+
+-- | This function can return a chord as first guess and a game state without that guess,
+-- it takes no argument as input and returns a tuple.
+initialGuess :: ([String], GameState)
+initialGuess = (generateChord, deleteElem generateChord generateAllChord)
 
 
-nextGuess :: ([String], GameState) -> (Int, Int, Int) -> ([String], GameState)
+nextGuess :: ([String], GameState) -> Answer -> ([String], GameState)
+nextGuess (guess, gs) answer = 
+
+
+generateNextGuess:: ([String], GameState) -> Answer -> ([String, GameState])
+generateNextGuess (guess, gs) answer = 
+
+-- | The first argument is the target, the second is the answer of the previous guess.
+-- It return a new GameState by filtering out the chords that can not yield such answer from
+-- origin GameState.
+reduceGameState :: [String] -> Answer -> GameState -> GameState
+reduceGameState target answer [] = []
+reduceGameState target answer (x:xs)
+  | response target x == answer = x : reduceGameState target answer xs
+  | otherwise = reduceGameState target answer xs
+
+-- | The first argument is the target, it will return a list of all the answers that
+-- the elements in the list can get, there may be duplicates.
+getAnswerOfGameState :: [String] -> GameState -> [Answer]
+getAnswerOfGameState target [] = []
+getAnswerOfGameState target (x:xs) = response target x : getAnswerOfGameState target xs
+
+-- | This function takes a list as input and can group same element, the output is a list of
+-- tuple, the first element in the tuple is the frequency of that element, and the second
+-- element in the tuple is that list element.
+-- This function should be used with expecation to generate the expectation of the game state.
+groupResult :: (Ord a) => [a] -> [(a, Int)]
+groupResult xs = toList (fromListWith (+) [(x, 1) | x <- xs])
+
+groupInt :: [(Int, a)] -> [Int]
+groupInt [] = []
+groupInt (x:xs) = fst x : groupInt xs
+
+expectation :: [Int] -> [Int] -> Double
+expectation [] l = 0
+expectation [a] [] = 0
+expectation (x:xs) l = fromIntegral(x*x) / fromIntegral(sum(l)) + expectation xs l
+
+
+
+
+
+
 
 
 -- | Compute the correct answer to a guess.  First argument is the 
@@ -42,9 +103,8 @@ response target guess = (right, rightNote, rightOctave)
         rightNote   = num - (length $ deleteFirstsBy (eqNth 0) guess' target) 
                     - right
         rightOctave = num - (length $ deleteFirstsBy (eqNth 1) guess' target) 
-<<<<<<< HEAD
                     - right
-=======
-                    - right
-
->>>>>>> fbc5fd58f1991b237a8506b76d611bcfb8ed085a
+-- | eqNth n l1 l2 returns True iff element n of l1 is equal to 
+--   element n of l2.
+eqNth :: Eq a => Int -> [a] -> [a] -> Bool
+eqNth n l1 l2 = (l1 !! n) == (l2 !! n)
--- Test.hs
+++ Test.hs
@@ -1,3 +1,9 @@
+import Data.List
+import Data.Map (fromListWith, toList)
+
+type GameState = [[String]]
+type Answer = (Int, Int, Int)
+
 generatePitch :: [String]
 generatePitch = [x ++ y | x <- note, y <- octave]
   where note = ["A", "B", "C", "D", "E", "F", "G"]
@@ -5,3 +11,59 @@ generatePitch = [x ++ y | x <- note, y <- octave]
 
 generateChord :: [String]
 generateChord = take 3 generatePitch
+
+generateAllChord :: [[String]]
+generateAllChord = [[x, y, z] | x <- all, y <- all, z <- all, x < y, y < z]
+  where all = generatePitch 
+
+charToString :: [Char] -> [String]
+charToString x = [x]
+
+deleteElem :: Eq a => a -> [a] -> [a]
+deleteElem _ [] = []
+deleteElem a (x:xs)
+  | a == x = xs
+  | otherwise = x : deleteElem a xs
+
+
+initialGuess :: ([String], GameState)
+initialGuess = (generateChord, deleteElem generateChord generateAllChord)
+
+
+-- | Compute the correct answer to a guess.  First argument is the 
+--   target, second is the guess.
+response :: [String] -> [String] -> (Int,Int,Int)
+response target guess = (right, rightNote, rightOctave)
+  where guess'      = nub guess
+        right       = length $ intersect guess' target
+        num         = length guess'
+        rightNote   = num - (length $ deleteFirstsBy (eqNth 0) guess' target) 
+                    - right
+        rightOctave = num - (length $ deleteFirstsBy (eqNth 1) guess' target) 
+                    - right
+-- | eqNth n l1 l2 returns True iff element n of l1 is equal to 
+--   element n of l2.
+eqNth :: Eq a => Int -> [a] -> [a] -> Bool
+eqNth n l1 l2 = (l1 !! n) == (l2 !! n)
+
+reduceGameState :: [String] -> Answer -> GameState -> GameState
+reduceGameState target answer [] = []
+reduceGameState target answer (x:xs)
+  | response target x == answer = x : reduceGameState target answer xs
+  | otherwise = reduceGameState target answer xs
+
+getAnswerOfGameState :: [String] -> GameState -> [Answer]
+getAnswerOfGameState target [] = []
+getAnswerOfGameState target (x:xs) = response target x : getAnswerOfGameState target xs
+
+groupResult :: (Ord a) => [a] -> [(a, Int)]
+groupResult xs = toList (fromListWith (+) [(x, 1) | x <- xs])
+
+groupInt :: [(Int, a)] -> [Int]
+groupInt [] = []
+groupInt (x:xs) = fst x : groupInt xs
+
+expectation :: [Int] -> [Int] -> Double
+expectation [] l = 0
+expectation [a] [] = 0
+expectation (x:xs) l = fromIntegral(x*x) / fromIntegral(sum(l)) + expectation xs l
